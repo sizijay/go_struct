3984286d31dd5f05885a5ebc9b64a28be2449355
sizijay <siziroxz@gmail.com>
2020-03-05 13:30:20 +0530

    Pointers added

--- main.go
+++ main.go
@@ -2,16 +2,37 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email string
+	phone int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contact   contactInfo
 }
 
 func main() {
 	// var bob person
-	bob := person{"Bobby", "Anderson"}
+	bob := person{"Bobby", "Anderson", contactInfo{"bob@gmail.com", 7394739}}
 	fmt.Println(bob)
 	fmt.Printf("%+v \n", bob)
-	fmt.Printf("First Name: %v", bob.firstName)
+	fmt.Printf("First Name: %v\nPhone: %v\n", bob.firstName, bob.contact.phone)
 	fmt.Println()
+
+	bob.print()
+
+	fmt.Println("\nUpdating First Name...")
+	bobP := &bob
+	bobP.updateFName("Ginny")
+	bob.print()
+}
+
+func (p person) print() {
+	fmt.Printf("\n****Printer Working******\n\nFirst Name\t:\t%v\nLastName\t:\t%v\nEmail\t\t:\t%v\nPhone\t\t:\t%v\n\n", p.firstName, p.lastName, p.contact.email, p.contact.phone)
+}
+
+func (p *person) updateFName(newFName string) {
+	p.firstName = newFName
 }
