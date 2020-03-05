85710722fd345e23d91b0a1e849adae9116fe875
sizijay <siziroxz@gmail.com>
2020-03-05 16:47:38 +0530

    interface introduced

---
+++ interface/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type englishBot struct{}
+type spanishBot struct{}
+
+type bot interface {
+	getGreeting() string
+}
+
+func (eb englishBot) getGreeting() string {
+	return "Hello!"
+}
+
+func (sb spanishBot) getGreeting() string {
+	return "Hola!"
+}
+
+func printGreeting(b bot) {
+	fmt.Println(b.getGreeting())
+}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	fmt.Print("English bot says: ")
+	printGreeting(eb)
+	fmt.Println()
+
+	fmt.Print("Spanish bot says: ")
+	printGreeting(sb)
+	fmt.Println()
+}
--- map/main.go
+++ map/main.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello")
+	colors := map[string]string{
+		"red": "#FF0000	",
+		"green": "#008000	",
+		"white": "#000000	",
+		"yellow": "#FFFF00	",
+		"orange": "#FFA500	",
+	}
+
+	print(colors)
+}
+
+func print(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("The hex code for the color " + color + " is " + hex)
+	}
 }
